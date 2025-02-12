1ef552b789a0f736541bccc94b55f02716c0c02e
xwizardcoder <167189077+xwizardcoder@users.noreply.github.com>
2025-02-12 13:12:48 +0530

    updated backed url

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chat-app-backend.onrender.com");
+
 
 const Chat = () => {
   const [username, setUsername] = useState("");
