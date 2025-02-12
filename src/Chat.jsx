f0bc00f89211ee5b889711c0e6fcac385a989856
xwizardcoder <167189077+xwizardcoder@users.noreply.github.com>
2025-02-12 19:10:34 +0530

    new data

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001"); // Connect to backend
+const socket = io("https://chat-app-backend-kvih.onrender.com"); // Connect to backend
 
 const Chat = ({ username }) => {
   const [message, setMessage] = useState("");
